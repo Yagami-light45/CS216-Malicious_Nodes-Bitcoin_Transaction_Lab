c0ed6056b975c56212dd851bf3c4002c70e2cf57
S Varshith Reddy <svarshithreddy9121@gmail.com>
2026-03-08 00:53:50 +0530

    Add: Funding address A

--- src/part2_segwit.py
+++ src/part2_segwit.py
@@ -9,6 +9,14 @@ RPC_PASSWORD = "varshith@CS216"
 RPC_HOST = "127.0.0.1"
 RPC_PORT = 18443
 
+
+ 
+# RPC Connection function
+def rpc_connection():
+    rpc_url = f"http://{RPC_USER}:{RPC_PASSWORD}@{RPC_HOST}:{RPC_PORT}"
+    return AuthServiceProxy(rpc_url)
+
+
 # Response class for setup function
 class setup_response:
     def __init__(self, success, message, data):
@@ -16,13 +24,10 @@ class setup_response:
         self.message = message
         self.data = data
 
-# RPC Connection function
-def rpc_connection():
-    rpc_url = f"http://{RPC_USER}:{RPC_PASSWORD}@{RPC_HOST}:{RPC_PORT}"
-    return AuthServiceProxy(rpc_url)
 
 # Setup function for wallets and addresses
-def setup_wallet_addresses(rpc):
+def setup_wallet_addresses(rpc)-> setup_response:
+    print("Setting up wallet and addresses")
     wallet_name = "part-2"
 
     try:
@@ -62,6 +67,32 @@ def setup_wallet_addresses(rpc):
         }
     )
 
+# Response class for funding function
+class funding_response:
+    def __init__(self, success, message, data):
+        self.success=success
+        self.message=message
+        self.data=data
+        
+        
+# Funding function for address A
+def fund_wallet(wallet_rpc, address_A)-> funding_response:
+    print("Funding wallet address A")
+    try:
+        block_hashes = wallet_rpc.generatetoaddress(101, address_A)
+        print(f"Mined {len(block_hashes)} blocks")
+
+        balance = wallet_rpc.getbalance()
+        print(f"Wallet balance: {balance} BTC")
+
+        utxos = wallet_rpc.listunspent(1, 9999999, [address_A])
+        print(f"\nUTXOs available for Address A': {len(utxos)}")
+        if utxos:
+            print(f"  Sample UTXO - txid: {utxos[0]['txid'][:16]}..., amount: {utxos[0]['amount']} BTC")
+    except JSONRPCException as err:
+        print(f"Error while funding address A: {err}")
+        return funding_response(False,"RPC request error", {})
+    return funding_response(True, "Funding succesful",utxos)
 
 def main():
     print(" CS 216: Bitcoin Transaction Lab - Part 2: P2SH-SegWit (P2SH-P2WPKH)")
@@ -81,7 +112,14 @@ def main():
     else:
         print(setup_response.message)
 
+    # Step-3 Fund the first address for transactions
+    fund_response=fund_wallet(setup_response.data["wallet"], setup_response.data["address_A"])
+    if not fund_response.success:
+        print(f"Funding error: {fund_response.message}")
+    else:
+        print(fund_response.message)
     
+    # Step-4 
     
 if __name__=="__main__" :
     main()
